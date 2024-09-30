63e61c74b2313e50c07bb5103eba2d693a33afae
giorgio <giorgiosaldana99@gmail.com>
2024-09-30 10:53:55 +0000

    [edit] Completed the subtask4 from task2 about the assignment

--- assignment/mim_gold.py
+++ assignment/mim_gold.py
@@ -28,6 +28,12 @@ def compute_task3(reader: TaggedCorpusReader):
     # Compute the 10 most frequent tokens in the MIM-GOLD.sent file
     return FreqDist(reader.words('MIM-GOLD.sent')).most_common(10)
 
+
+def compute_task4(reader: TaggedCorpusReader):
+    # Compute the 20 most frequent PoS tags in the MIM-GOLD.sent file
+    return FreqDist(tag for (word, tag) in reader.tagged_words('MIM-GOLD.sent')).most_common(20)
+
+
 def main():
     reader = TaggedCorpusReader("./", r'.*\.sent', encoding='utf-8')
     task1 = compute_task1(reader)
@@ -39,9 +45,10 @@ def main():
     print(f"Number of types: {task2['num_types']}\n")
 
     task3 = compute_task3(reader)
-    print(f"The most 10 frequent tokens \n {chr(10).join([f'{item[0]} => {item[1]}' for item in task3])}\n")
+    print(f"The most 10 frequent tokens \n{chr(10).join([f'{item[0]} => {item[1]}' for item in task3])}\n")
 
-
+    task4 = compute_task4(reader)
+    print(f"The most 20 frequent PoS tags: \n{chr(10).join([f'{item[0]} => {item[1]}' for item in task4])}\n")
 
 if __name__ == "__main__":
     main()
